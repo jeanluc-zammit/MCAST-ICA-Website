d133de09311e26a8b26ee4e6b4fec8e9da2984aa
Jeanluc Zammit <jeanpowerson@Jeanlucs-MBP.lan>
2018-04-21 14:14:26 +0200

    Added main files

---
+++ application/views/HomePage.php
@@ -0,0 +1,149 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?=base_url('css/styles.css')?>">
+	<link type="text/css" rel="stylesheet" href="css/font-awesome.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+<!--    <link href="css/bootstrap.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<title>Institute of Creative Arts</title>
+</head>
+<body>
+
+	                                           <!--HEADER START-->
+
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+              <img src="images/MCAST-Main.png" alt = "Mcast Logo" title = "Logo" class="logo"/>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Academic Calendar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Student Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Register</a>
+              </li>
+        </ul>
+<!--
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+-->
+  </div>
+</nav>
+
+                                            <!-- HEADER END-->
+
+                                            <!--CAROUSEL START-->
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="width: 1500px;">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/SLIDE2.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <br>
+</div>
+
+                                            <!--CAROUSEL END-->
+
+                                        <!--MAIN CONTENT START-->
+
+<div class="container">
+    <div class="col-lg-3">
+        <h1>
+            <img src="images/imagethumb.png" class="rounded" alt="..." width="60%">
+
+        </h1>
+    </div>
+
+    <div class="col-lg-3">
+        <h1>
+            <img src="images/imagethumb.png" class="rounded" alt="..." width="60%">
+        </h1>
+    </div>
+
+<br>
+</div>
+
+                                        <!--MAIN CONTENT END->
+
+
+		                                <!--FOOTER START-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+          <img src="images/solid_inverse.png" alt = "Mcast Logo" title = "Logo" class="logo"/>
+      </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mx-auto mt-5 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">News & Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Our Services</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+         <p> <i class="fab fa-facebook-f" style="color:#CCCECF;"></i> <span style="color:#CCCECF;">Facebook</span> </p>
+         <p> <i class="fab fa-twitter" style="color:#CCCECF;"></i> <span style="color:#CCCECF;">Twitter</span> </p>
+        <p>  <i class="fab fa-instagram" style="color:#CCCECF;"></i> <span style="color:#CCCECF;">Instagram</span> </p>
+        <p>  <i class="fab fa-youtube" style="color:#CCCECF;"></i> <span style="color:#CCCECF;">YouTube</span> </p>
+      </li>
+
+    </ul>
+
+  </div>
+</nav>
+		                                  <!--FOOTER END-->
+</div>
+<!-- End of a wrapper -->
+</body>
+</html>
---
+++ application/views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Login - MCAST ICA</title>
+        <link rel="stylesheet" type="text/css" href="<?=base_url('css/styles.css')?>">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+
+        <img src="<?=base_url('images/MCAST-Main.png')?>" alt = "Mcast Logo" class="logo_login"/> <!--use base_url otherwise it wouldnt find the image-->
+
+        <div class="container">
+            <div class="input-group mb-3">
+                <input type="text" placeholder="Email Address" class="form-control" aria-label="Amount (to the nearest dollar)">
+            </div>
+        </div>
+
+
+    </body>
+</html>
